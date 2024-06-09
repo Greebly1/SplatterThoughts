492732d8b2361eae1b7b692178bdb38a582cb9df
Greebly1 <dakotat103@gmail.com>
2024-06-09 15:15:43 -0800

    Debug Plugin
    press 'f3' to cycle debug menu in game

--- PaintTest.gd
+++ PaintTest.gd
@@ -8,6 +8,7 @@ var paintBuffer : SubViewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Engine.max_fps = 60 
 	paintBuffer.render_target_clear_mode = SubViewport.CLEAR_MODE_NEVER;
 	pass # Replace with function body.
 
